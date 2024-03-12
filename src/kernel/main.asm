a1975625b74a31f0f34f6cc9807126ead13e2e09
xiayulin123 <xiayulinsky@gmail.com>
2024-03-11 22:12:51 -0400

    file system

--- src/kernel/main.asm
+++ src/kernel/main.asm
@@ -1,61 +1,66 @@
 org 0x7C00
 bits 16
 
+
 %define ENDL 0x0D, 0x0A
 
+
 start:
-	jmp main
+    jmp main
+
 
 ;
-; Print a string to  the screen
+; Prints a string to the screen
 ; Params:
-;	-ds:si points to string
+;   - ds:si points to string
 ;
-
 puts:
-	;save registers we will modify
-	push si
-	push ax
-	push bx
+    ; save registers we will modify
+    push si
+    push ax
+    push bx
 
 .loop:
-	lodsb		;loads next character in al
-	or al, al 	;verify if next character is null?
-	jz .done
+    lodsb               ; loads next character in al
+    or al, al           ; verify if next character is null?
+    jz .done
 
-	mov ah, 0x0E  	;call bios interrupt
-	mov bh, 0
-	int 0x10
-	jmp .loop
-	
+    mov ah, 0x0E        ; call bios interrupt
+    mov bh, 0           ; set page number to 0
+    int 0x10
+
+    jmp .loop
+
 .done:
-
-	pop bx
-	pop ax
-	pop si
-	ret
-
-
-
+    pop bx
+    pop ax
+    pop si    
+    ret
+    
 
 main:
-	; setup data segments
-	mov ax, 0
-	mov ds, ax
-	mov es, ax
+    ; setup data segments
+    mov ax, 0           ; can't set ds/es directly
+    mov ds, ax
+    mov es, ax
+    
+    ; setup stack
+    mov ss, ax
+    mov sp, 0x7C00      ; stack grows downwards from where we are loaded in memory
 
-	;setup stack
-	mov ss, ax
-	mov sp, 0x7C00 	;stack grows downwards from where we loaded in memory
+    ; print hello world message
+    mov si, msg_hello
+    call puts
 
-	mov si, msg_hello
-	call puts
-	hlt
+    hlt
 
 .halt:
-	jmp .halt
+    jmp .halt
 
-msg_hello: db 'Hello world', ENDL, 0
+
+
+msg_hello: db 'Hello world from kernel!', ENDL, 0
+
 
 times 510-($-$$) db 0
 dw 0AA55h
